ef96d27b537adb7920cc809bcf05c8f2672f9e13
diyya23 <rajajeevanand7@gmail.com>
2023-11-07 01:46:07 +0530

    add modules

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const myTime = setTimeout(message, 2000)
+// const myTime = setTimeout(message, 2000)
 
-function message() {
-    console.log("Hiii")
-}
+// function message() {
+//     console.log("Hiii")
+// }
 
-function timeOut() {
-    clearTimeout(myTime)
-}
+// function timeOut() {
+//     clearTimeout(myTime)
+// }
 
-message()
-timeOut()
+// message()
+// timeOut()
+
+var logger = require('./simple')
+logger.log('message')
---
+++ simple.js
@@ -0,0 +1,8 @@
+var url = "http://simple.io/rj"
+
+function log(message) {
+    // send HTTP request 
+    console.log(message)
+}
+
+module.exports.log = log
