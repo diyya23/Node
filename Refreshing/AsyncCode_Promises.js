1fdbf6a4052b2858680304d437c822ee4cb214e9
diyya23 <rajajeevanand7@gmail.com>
2023-11-09 20:58:29 +0530

    added promises

--- Refreshing/AsyncCode_Promises.js
+++ Refreshing/AsyncCode_Promises.js
@@ -17,21 +17,39 @@
 
 // Lets Create a Awesome SetTimeout Feature!!!
 
-const display = callback => {
-    setTimeout(() => {
-        console.log('RJ')
-        setTimeout(() => {
-            console.log('Wait.. for 2 Seconds.......')
-        }, 2000)
-        setTimeout(() => {
-            console.log('Thank You :)')
-        }, 1000);
-    }, 1500)
-}
+// const display = callback => {
+//     setTimeout(() => {
+//         console.log('RJ')
+//         setTimeout(() => {
+//             console.log('Thank You :)')
+//         }, 2000)
+//         setTimeout(() => {
+//             console.log('Wait for 2 Seconds....')
+//         }, 1000);
+//     }, 1500)
+// }
 
-setTimeout(() => {
-    console.log("My, Name is: ")
-    display(text => {
+// setTimeout(() => {
+//     console.log("My, Name is: ")
+//     display(text => {
+//         console.log(text)
+//     })
+// }, 2000);
+
+
+// ######################### Promises ###############################
+let mypromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Done')
+    }, 1500)
+})
+
+setTimeout(() => {    
+    console.log('My Name is: ')
+    mypromise.then(text => {
+        console.log(text) 
+        return text
+    }).then(text => {
         console.log(text)
     })
 }, 2000);
