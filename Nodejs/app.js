1e3b2785ab6af93a05a81cf14e1d96f65f2a5154
diyya23 <rajajeevanand7@gmail.com>
2023-11-10 18:45:12 +0530

    added Routing requests

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,13 +1,22 @@
-const http = require('http')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.headers, req.method);  
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>NodeJs</title></head>');
-    res.write('<body></h3>This is my NodeJS Server.....</h3></body>');
-    res.write('<html>');
-    res.end();
-})
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter the text</title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+    res.write("<html>");
+    return res.end();
+  }
 
-server.listen(4000)
+  console.log(req.url, req.headers, req.method);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>NodeJs</title></head>");
+  res.write("<body></h3>This is my NodeJS Server.....</h3></body>");
+  res.write("<html>");
+  res.end();
+});
+
+server.listen(3000);
