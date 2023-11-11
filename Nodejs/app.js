ee940d2c0bcf941ce7904683291b57c7b5efaa12
diyya23 <rajajeevanand7@gmail.com>
2023-11-11 18:52:44 +0530

    Added redirecting requests

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,13 +1,23 @@
 const http = require("http");
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter the text</title></head>");
     res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
     res.write("<html>");
     return res.end();
+  }
+
+  if(url === '/message' && method === 'POST') {
+    fs.writeFileSync('message', 'this text from editor!!!')
+    res.statusCode = 302
+    res.setHeader = ('loaction', '/')
+    return res.end()
   }
 
   console.log(req.url, req.headers, req.method);
